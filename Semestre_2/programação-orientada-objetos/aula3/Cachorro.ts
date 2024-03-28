4ead97a200e22a5e833cd86fdddfb0dd98b12b1e
marce1in <marcelo.desenvolvedor.contato@gmail.com>
2024-03-28 11:29:40 -0300

    aula 3 oop

---
+++ Semestre_2/programação-orientada-objetos/aula3/Cachorro.ts
@@ -0,0 +1,67 @@
+import { Util } from "./Util";
+
+export class Cachorro {
+
+    raca: string;
+    nome: string;
+    porte: string;
+    pedigree: boolean;
+    peso: number;
+    dataNascimento: Date;
+    energia: number;
+    felicidade: number;
+    adestrado: boolean;
+
+    constructor(nome: string) {
+        this.raca = 'indefinido';
+        this.nome = nome;
+        this.porte = 'indefinido';
+        this.pedigree = false;
+        this.energia = (Math.floor( Math.random() * 99));
+        this.peso = Math.random() * 1;
+        this.dataNascimento = new Date();
+        this.felicidade = Math.floor(1 + Math.random() * 99);    
+        this.adestrado = false;
+        
+    }
+    comer(): void {
+        this.peso *= 1.1
+        this.felicidade += parseInt(""+1+ Math.random() * 9);
+        this.energia -= parseInt(""+10+ Math.random() * 10);
+        this.validaStatus(this);
+     
+    }
+    latir(numeroDeVezes: number): string {
+        this.felicidade -= parseInt(""+1+ Math.random() * 2)*numeroDeVezes;
+        this.energia -= parseInt(""+1+ Math.random() * 4)*numeroDeVezes;
+        this.validaStatus(this);
+        return('Au '.repeat(numeroDeVezes));
+    }
+    dormir(tempoEmHoras: number): void {
+        this.energia += Util.randomizar(1,10)*tempoEmHoras;
+        this.felicidade += Util.randomizar(1,3)*tempoEmHoras;
+        this.validaStatus(this);
+        
+    }
+    brincar(tempoEmMinutos: number): void {
+        this.energia -= Util.randomizar(1,4)*tempoEmMinutos;
+        this.felicidade += Util.randomizar(1,4)*tempoEmMinutos;
+        this.validaStatus(this);
+    }
+    validaStatus(cachorro: Cachorro): void{
+        if(cachorro.energia < 0){
+            throw new Error(`O ${this.nome} subiu!`);
+        }
+        if(cachorro.felicidade < 0){
+            console.log(`${this.nome} está deprimido!`);
+        }
+        if(cachorro.energia > 100){
+            cachorro.energia = 100;
+        }
+        if(cachorro.felicidade > 100){
+            cachorro.felicidade = 100;
+        }
+    }
+    
+
+}
---
+++ Semestre_2/programação-orientada-objetos/aula3/Personagem.ts
@@ -0,0 +1,8 @@
+export class Personagem{
+  //atributos, campos ou propriedades
+  nome:string = '';
+  energia:number = 0;
+  ataque:number = 0;
+  defesa:number = 0;
+
+}

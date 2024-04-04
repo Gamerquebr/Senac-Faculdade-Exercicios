ceac68caa514bd63e5e372f9a08f997a56cffd02
marce1in <marcelo.desenvolvedor.contato@gmail.com>
2024-04-04 11:57:32 -0300

    update aula 5 oop exercício

--- Semestre_2/programação-orientada-objetos/aula3/Cachorro.test.ts
+++ Semestre_2/programação-orientada-objetos/aula3/Cachorro.test.ts
@@ -2,5 +2,10 @@ import { Cachorro } from "./Cachorro";
 let cachorro: Cachorro = new Cachorro("glad")
 
 describe('Comer', () => {
-    it('Comer deve aumentar o peso')
+    it('Comer deve aumentar o peso', () => {
+        const peso_inicial: number= cachorro.peso
+        cachorro.comer
+        const peso_depois: number = cachorro.peso
+        expect((peso_depois).toBe(peso_inicial*1.1))
+    })
 })

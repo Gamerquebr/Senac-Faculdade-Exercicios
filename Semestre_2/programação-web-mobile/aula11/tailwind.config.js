8b39a1cfb5d338f36224190336019a04f63b23aa
Marcel1n <marcelo.desenvolvedor.contato@gmail.com>
2024-06-07 11:25:36 -0300

    aula11 tailwind dev web mobile

---
+++ Semestre_2/programação-web-mobile/aula11/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        fundo:'#F6F6F6',
+        azulbotao:'#23A6F0',
+      }
+    },
+  },
+  plugins: [],
+}
